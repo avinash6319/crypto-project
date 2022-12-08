285c2fde4a0c283bca5a725d32efdcde72075d22
AVINASH SINGH <avinashsingh93690@gmail.com>
2022-12-08 22:50:17 +0530

    final

---
+++ index.js
@@ -0,0 +1,28 @@
+const express= require('express')
+const { default: mongoose } = require('mongoose')
+const route= require('./routes/route')
+const app = express()
+
+app.use(express.json())
+mongoose
+  .connect(
+    "mongodb+srv://avinash01:avikumarsingh@avinash.qmdqwkw.mongodb.net/test",
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => console.log("MongoDb is Connected."))
+  .catch((error) => console.log(error));
+
+
+app.use("/",route)
+
+app.use(function (req, res) {
+  var err = new Error("Not Found.")
+  err.status = 404
+  return res.status(404).send({ status: "404", msg: "Path not Found." })
+})
+
+app.listen(process.env.PORT ||3000,function(){
+    console.log('Express App Running on Port: ' + (process.env.PORT || 3000))
+})
